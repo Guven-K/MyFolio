258f3bcaf68e5b9c7f49ec450a5878ce71542ca7
Guven <mukremin@live.co.uk>
2025-01-03 23:19:47 +0000

    Revert "gitignore"
    This reverts commit 1270cc187d5f644f292c9e7c09f83d8c22b38f81.

--- MyFolio/obj/Debug/net9.0/MyFolio.AssemblyInfo.cs
+++ MyFolio/obj/Debug/net9.0/MyFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26894eb4a49f55b27a36f4f2f6f17988c29451c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

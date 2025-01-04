ddb79a9dbcc1f75f9a29a37504f19cede5723706
Guven <mukremin@live.co.uk>
2025-01-04 01:37:02 +0000

    Test changes

--- MyFolio/ViewModels/MainWindowViewModel.cs
+++ MyFolio/ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,14 @@
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to MyFolio!";
+    public string Greeting { get; set; } = "Welcome to MyFolio!";
+
+    public MainWindowViewModel()
+    {
+        
+    }
+    public void Test()
+    {
+        Greeting = "Let's test this!";
+    }
 }

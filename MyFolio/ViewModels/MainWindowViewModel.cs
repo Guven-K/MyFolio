4c43d1148bed180124a0ec2775abe37a75206f64
Guven <mukremin@live.co.uk>
2025-03-03 19:14:35 +0000

    Behaviours Package, Picture Picture Mouse Over control

--- MyFolio/ViewModels/MainWindowViewModel.cs
+++ MyFolio/ViewModels/MainWindowViewModel.cs
@@ -3,9 +3,12 @@ using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
 using Avalonia;
 using Avalonia.Platform;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 using CommunityToolkit;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.IO;
 
 namespace MyFolio.ViewModels;
 
@@ -18,20 +21,30 @@ public partial class MainWindowViewModel : ViewModelBase
 
     [ObservableProperty]
     private Bitmap? _profilePicture;
+
+    [ObservableProperty]
+    private bool _isHovering;
     #endregion
+
 
     #region Constructor
     public MainWindowViewModel()
     {
         Test();
         Greeting = "Hello, World!";
+        DefaultProfilePicture();
 
-        var uri = new Uri("avares://MyFolio/Assets/Images/profile.png");
-        ProfilePicture = new Bitmap(AssetLoader.Open(uri));
+
     }
     #endregion
 
     #region Methods
+
+    private void DefaultProfilePicture()
+    {
+        var uri = new Uri("avares://MyFolio/Assets/Images/profile.png");
+        ProfilePicture = new Bitmap(AssetLoader.Open(uri));
+    }
 
     public async void Test()
     {
@@ -44,6 +57,7 @@ public partial class MainWindowViewModel : ViewModelBase
         await Task.Delay(2000);
         Greeting = "Are you ready?";
     }
+
     #endregion
 
 
@@ -54,5 +68,44 @@ public partial class MainWindowViewModel : ViewModelBase
     {
         Greeting = "You clicked me!";
     }
+
+
+    [RelayCommand]
+    public async Task ChangeProfilePicture()
+    {
+        var openDialog = new OpenFileDialog
+        {
+            AllowMultiple = false,
+            Title = "Select Profile Picture",
+            Filters =
+                {
+                    new FileDialogFilter { Name = "Image Files", Extensions = { "png", "jpg", "jpeg", "bmp" } }
+                }
+        };
+
+        var window = Application.Current?.ApplicationLifetime is Avalonia.Controls.ApplicationLifetimes.IClassicDesktopStyleApplicationLifetime desktop
+            ? desktop.MainWindow
+            : null;
+
+        var result = await openDialog.ShowAsync(window);
+        if (result != null && result.Length > 0)
+        {
+            var selectedFile = result[0];
+            try
+            {
+                await using var stream = File.OpenRead(selectedFile);
+                ProfilePicture = new Bitmap(stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading profile picture: {ex.Message}");
+            }
+        }
+
+    }
+
+    public void OnMouseEnter(object? sender, Avalonia.Input.PointerEventArgs e) => IsHovering = true;
+    public void OnMouseLeave(object? sender, Avalonia.Input.PointerEventArgs e) => IsHovering = false;
+
     #endregion
 }
